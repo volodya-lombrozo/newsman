6b067ae180ea7bebcbaef3293d5865dbc2285179
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-01 12:05:36 +0300

    chore: find link inside pdd issue

--- lib/newsman/issues.rb
+++ lib/newsman/issues.rb
@@ -15,3 +15,21 @@ class Issue
   end
 end
 
+class PddIssue
+
+  def initialize(title, body, repo, number)
+    @title = title
+    @body = body
+    @repo = repo
+    @number = number
+  end
+
+  def extract_real_body
+     return @body[/https:\/\/github\.com\/[\w\-\/]+\/blob\/[\w\d]+\/[\w\/\.\-]+#\w+-\w+/, 0]
+  end
+
+  def to_s
+    "title: ```#{@title}```,\ndescription: ```#{extract_real_body}```,\nrepo: ```#{@repo}```,\nissue number: \##{@number}\n"
+  end
+end
+
